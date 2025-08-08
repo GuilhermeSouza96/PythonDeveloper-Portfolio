2056da188d6e9a3daba82010b13ccefd5ec31190
Guilherme Albuquerque <souza1102@hotmail.com>
2025-08-08 16:16:30 -0300

    Update settings.py

--- School_Project/setup/settings.py
+++ School_Project/setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0qk+kp791=qbcvga)o=8$i@c-r5)b_o2nca=+$tnh&n$h&*+x$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
