36904a80789da397643cceac242eb0f0c69341c3
Guilherme Souza <souza1102@hotmail.com>
2025-08-08 16:15:42 -0300

    Atualizando arquivos com nova versão

--- School_Project/school/views.py
+++ School_Project/school/views.py
@@ -2,9 +2,29 @@ from school.models import Student, Course, Enrollment
 from school.serializers import StudentSerializer, CourseSerializer, EnrollmentSerializer, ListEnrollmentsCourseSerializer, ListEnrollmentsStudentSerializer, StudentSerializerV2
 from rest_framework import viewsets, generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.throttling import UserRateThrottle
+from school.throttles import EnrollmentAnonRateThrottle
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class StudentViewSet(viewsets.ModelViewSet):
-    queryset = Student.objects.all()
+    '''
+    ViewSet Description:
+    - Endpoint for student CRUD.
+    Sort Fields:
+    - Name: Allows you to sort results by name.
+
+    Search Fields:
+    - Name: Allows you to search results by name.
+    - CPF: Allows you to search results by CPF.
+
+    Allowed HTTP Methods:
+    - GET, POST, PUT, PATCH, DELETE
+
+    Serializer Class:
+    - EstudanteSerializer: Used for data serialization and deserialization.
+    - If the API version is 'v2', use EstudanteSerializerV2.
+    '''
+    queryset = Student.objects.all().order_by('id')
     # serializer_class = StudentSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['name']
@@ -16,21 +36,54 @@ class StudentViewSet(viewsets.ModelViewSet):
         return StudentSerializer
 
 class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Course.objects.all()
+    '''
+    ViewSet Description:
+    - Endpoint for course CRUD.
+
+    Allowed HTTP Methods:
+    - GET, POST, PUT, PATCH, DELETE
+    '''
+    queryset = Course.objects.all().order_by('id')
     serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class EnrollmentViewSet(viewsets.ModelViewSet):
-    queryset = Enrollment.objects.all()
+    '''
+    ViewSet Description:
+    - Endpoint for enrollment CRUD.
+
+    Allowed HTTP Methods:
+    - GET, POST
+
+    Throttle Classes:
+    - EnrollmentAnonRateThrottle: Rate limit for anonymous users.
+    - UserRateThrottle: Rate limit for authenticated users.
+    '''
+    queryset = Enrollment.objects.all().order_by('id')
     serializer_class = EnrollmentSerializer
+    throttle_classes = [UserRateThrottle, EnrollmentAnonRateThrottle]
+    http_method_names = ['get', 'post']
 
 class ListEnrollmentStudent(generics.ListAPIView):
+    '''
+    View Description:
+    - List Enrollments by Student id
+    Params:
+    - pk (int): Primary object identifyer. Must be integer
+    '''
     def get_queryset(self):
-        queryset = Enrollment.objects.filter(student_id = self.kwargs['pk'])
+        queryset = Enrollment.objects.filter(student_id = self.kwargs['pk']).order_by('id')
         return queryset
     serializer_class = ListEnrollmentsStudentSerializer
 
 class ListEnrollmentCourse(generics.ListAPIView):
+    '''
+    View Description:
+    - List Enrollments by Student id
+    Params:
+    - pk (int): Primary object identifyer. Must be integer
+    '''
     def get_queryset(self):
-        queryset = Enrollment.objects.filter(course_id = self.kwargs['pk'])
+        queryset = Enrollment.objects.filter(course_id = self.kwargs['pk']).order_by('id')
         return queryset
     serializer_class = ListEnrollmentsCourseSerializer
--- School_Project/setup/urls.py
+++ School_Project/setup/urls.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from school.views import StudentViewSet, CourseViewSet, EnrollmentViewSet, ListEnrollmentStudent, ListEnrollmentCourse
 from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Documentation",
+      default_version='v1',
+      description="School's API Documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 router = routers.DefaultRouter()
 router.register('students', StudentViewSet, basename = 'Students')
@@ -13,4 +27,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('students/<int:pk>/enrollments', ListEnrollmentStudent.as_view()),
     path('course/<int:pk>/enrollments', ListEnrollmentCourse.as_view()),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
